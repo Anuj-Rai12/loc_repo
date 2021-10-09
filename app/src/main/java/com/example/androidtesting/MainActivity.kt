d4eb7c95f4cfdb8c2d976b12c865a52786d96e36
Anuj <anujrai04082002.0@gmail.com>
2021-10-09 18:05:24 +0530

    Add Location getting Feature status:Successfully

--- app/src/main/java/com/example/androidtesting/MainActivity.kt
+++ app/src/main/java/com/example/androidtesting/MainActivity.kt
@@ -1,47 +1,150 @@
 package com.example.androidtesting
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.LocationManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.androidtesting.databinding.ActivityMainBinding
 import com.example.androidtesting.utils.*
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.vmadalin.easypermissions.EasyPermissions
 import com.vmadalin.easypermissions.dialogs.SettingsDialog
+
 
 const val TAG = "ANUJ"
 
 class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
     private lateinit var binding: ActivityMainBinding
+    private var dialog: Dialog? = null
+    private var data: UserLocation? = null
+    private val mFusedLocation by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
+    }
+
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         getLocationData()
+        binding.locationClick.setOnClickListener {
+            if (getLocationData()) {
+                if (isLocationEnabled()) {
+                    callLocation()
+                } else
+                    this.msg("Please Turn On Location ", Toast.LENGTH_LONG)
+            } else {
+                this.msg("Permission is Not Given", Toast.LENGTH_SHORT)
+                getLocationData()
+            }
+        }
+        binding.shareClick.setOnClickListener {
+            if (data != null) {
+                val url = "$URL${data?.latitude},${data?.longitude}"
+                val message="Hi check my location on ${data?.timestamp} \n\n $url"
+                this.shareText(title = "Share Location", message = message)
+            } else {
+                this.msg("Firstly Get your location")
+            }
+        }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun callLocation() {
+        mFusedLocation.lastLocation
+            .addOnCompleteListener { task ->
+                val location = task.result
+                if (location == null) {
+                    requestNewLocationData()
+                } else {
+                    setUpData(longitude = location.longitude, latitude = location.latitude)
+                }
+            }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest.create()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 5
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        // setting LocationRequest
+        // on FusedLocationClient
+//        mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
+        Looper.myLooper()
+            ?.let { mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, it) }
+
+    }
+
+    private val mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation = locationResult.lastLocation
+            setUpData(latitude = mLastLocation.latitude, longitude = mLastLocation.longitude)
+        }
+    }
+
+    private fun setUpData(latitude: Double, longitude: Double) {
+        UserLocation(
+            longitude = "$longitude",
+            latitude = "$latitude"
+        ).also {
+            data = it
+            dialog = Dialog(
+                "My Location",
+                msg = "The Longitude is ${it.longitude} \n And Latitude is ${it.latitude}\nAt ${it.timestamp}"
+            )
+            dialog?.show(supportFragmentManager, TAG)
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
 
     @RequiresApi(Build.VERSION_CODES.Q)
-    private fun getLocationData() {
-        if (!this.checkFinePermission()){
-            request(Manifest.permission.ACCESS_FINE_LOCATION, FINE_LOCATION,"Device")
+    private fun getLocationData(): Boolean {
+        if (!this.checkFinePermission()) {
+            request(Manifest.permission.ACCESS_FINE_LOCATION, FINE_LOCATION, "Device")
+            return false
         }
-        if (!this.checkCoarsePermission()){
-            request(Manifest.permission.ACCESS_COARSE_LOCATION, COARSE_LOCATION,"Location")
+        if (!this.checkCoarsePermission()) {
+            request(Manifest.permission.ACCESS_COARSE_LOCATION, COARSE_LOCATION, "Location")
+            return false
         }
-        if (!this.checkBackPermission()){
-            request(Manifest.permission.ACCESS_BACKGROUND_LOCATION, BACK_LOCATION,"Background Location")
+        if (!this.checkBackPermission()) {
+            request(
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                BACK_LOCATION,
+                "Background Location"
+            )
+            return false
         }
+        return true
     }
 
-    private fun request(manifest:String,code:Int,name:String)=EasyPermissions.requestPermissions(
-        this,
-        "Kindly Give us $name permission,otherwise application may not work Properly.",
-        code,
-        manifest,
-    )
+    private fun request(manifest: String, code: Int, name: String) =
+        EasyPermissions.requestPermissions(
+            this,
+            "Kindly Give us $name permission,otherwise application may not work Properly.",
+            code,
+            manifest,
+        )
 
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
@@ -51,6 +154,11 @@ class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
             } else
                 getLocationData()
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        dialog?.dismiss()
     }
 
     @RequiresApi(Build.VERSION_CODES.Q)
--- app/src/main/java/com/example/androidtesting/utils/UtilsFiles.kt
+++ app/src/main/java/com/example/androidtesting/utils/UtilsFiles.kt
@@ -2,13 +2,24 @@ package com.example.androidtesting.utils
 
 import android.Manifest
 import android.app.Activity
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
 import android.os.Build
+import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.example.androidtesting.R
 import com.vmadalin.easypermissions.EasyPermissions
+import java.text.DateFormat
 
-const val FINE_LOCATION=101
-const val COARSE_LOCATION=102
-const val BACK_LOCATION=201
+const val FINE_LOCATION = 101
+const val COARSE_LOCATION = 102
+const val BACK_LOCATION = 201
+
+const val URL = "https://www.google.com/maps/search/?api=1&query="
 fun Activity.checkCoarsePermission() =
     EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_COARSE_LOCATION)
 
@@ -18,3 +29,38 @@ fun Activity.checkBackPermission() =
 
 fun Activity.checkFinePermission() =
     EasyPermissions.hasPermissions(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+fun Context.msg(msg: String, time: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, msg, time).show()
+}
+
+class Dialog(private val title: String, private val msg: String) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alterDialog =
+            AlertDialog.Builder(requireActivity()).setTitle(title).setIcon(R.drawable.loction)
+        alterDialog.setMessage(msg)
+            .setPositiveButton("ok") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+        return alterDialog.create()
+    }
+}
+
+data class UserLocation(
+    val id: Int = 0,
+    val longitude: String,
+    val latitude: String,
+    val create: Long = System.currentTimeMillis()
+) {
+    val timestamp: String
+        get() = DateFormat.getDateTimeInstance().format(create)
+}
+
+
+fun Activity.shareText(title: String, message: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plan"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    startActivity(Intent.createChooser(intent, title))
+}
